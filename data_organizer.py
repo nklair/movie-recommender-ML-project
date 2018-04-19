3b7f92a74169f755e033ad1d975b4c3228a959f2
Connor McKendrick Higgins <chiggin7@crcfe01.crc.nd.edu>
2018-04-19 12:32:47 -0400

    pushing changes to similarity and data_organizer

--- data_organizer.py
+++ data_organizer.py
@@ -36,11 +36,17 @@ def organize_data_by_movie(filename, movie_dict):
 
 def create_csv(filename):
 	instance_list = []
-	users = pickle_open(filename)
-	for user in users.keys():
-		for movie in users[user].keys():
-			instance_list.append((int(user), int(movie), int(users[user][movie])))
+	movies = pickle_open(filename)
+	for movie in movies.keys():
+		for user in movies[movie].keys():
+			instance_list.append((int(user), int(movie), int(movies[movie][user])))
 	return instance_list
+	#instance_list = []
+	#users = pickle_open(filename)
+	#for user in users.keys():
+	#	for movie in users[user].keys():
+	#		instance_list.append((int(user), int(movie), int(users[user][movie])))
+	#return instance_list
 
 def save_shuffled_csv(filename, instance_list):
 	numpy.random.shuffle(instance_list)
@@ -60,21 +66,49 @@ def pickle_open(filename):
 	f = open(filename, "rb")
 	return pickle.load(f)
 
-if __name__ == "__main__":
-	#print("HI MIKE!")
+def pickle_by_user():
 	users = {}
-	movies = {}
 	organize_data_by_user("./netflix-prize-data/combined_data_1.txt", users)
 	organize_data_by_user("./netflix-prize-data/combined_data_2.txt", users)
 	organize_data_by_user("./netflix-prize-data/combined_data_3.txt", users)
 	organize_data_by_user("./netflix-prize-data/combined_data_4.txt", users)
+	average_user_ratings(users)
 	pickle_save(users, "user_movie_ratings.pickle")
 
+def pickle_by_movie():
+	movies = {}
 	organize_data_by_movie("./netflix-prize-data/combined_data_1.txt", movies)
 	organize_data_by_movie("./netflix-prize-data/combined_data_2.txt", movies)
 	organize_data_by_movie("./netflix-prize-data/combined_data_3.txt", movies)
 	organize_data_by_movie("./netflix-prize-data/combined_data_4.txt", movies)
 	pickle_save(movies, "movie_user_ratings.pickle")
 
-	instance = create_csv("/afs/crc.nd.edu/user/c/chiggin7/movie-recommender-ML-project/user_movie_ratings.pickle")
-	save_shuffled_csv("/afs/crc.nd.edu/user/c/chiggin7/movie-recommender-ML-project/shuffled_user_data.csv", instance)
+def get_top_n(movies, n):
+	movie_list = []
+	for movie in movies:
+		movie_list.append((len(movies[movie]), movie))
+		
+	movie_list.sort(key=lambda tup: tup[0], reverse=True)  # sorts in place
+	
+	new_dict = {}
+	for i in range(n):
+		new_dict[movie_list[i][1]] = movies[movie_list[i][1]]
+
+	return new_dict
+
+if __name__ == "__main__":
+	
+	#pickle_by_user()
+	#pickle_by_movie()
+
+	#movies = pickle_open("/afs/crc.nd.edu/user/c/chiggin7/movie-recommender-ML-project/full_movie_user_ratings.pickle")
+	#top_500 = get_top_n(movies, 500)
+	#pickle_save(top_500, "/afs/crc.nd.edu/user/c/chiggin7/movie-recommender-ML-project/movie_user_ratings.pickle")
+	#instance = create_csv("/afs/crc.nd.edu/user/c/chiggin7/movie-recommender-ML-project/movie_user_ratings.pickle")
+	#save_shuffled_csv("/afs/crc.nd.edu/user/c/chiggin7/movie-recommender-ML-project/shuffled_user_data.csv", instance)
+
+	# ****OLD**** instance = create_csv("/afs/crc.nd.edu/user/c/chiggin7/movie-recommender-ML-project/user_movie_ratings.pickle")
+	#instance = create_csv("/afs/crc.nd.edu/user/c/chiggin7/movie-recommender-ML-project/movie_user_ratings.pickle")
+	#save_shuffled_csv("/afs/crc.nd.edu/user/c/chiggin7/movie-recommender-ML-project/shuffled_user_data.csv", instance)
+
+
--- similarity.py
+++ similarity.py
@@ -1,41 +1,77 @@
-def similarity(movie1, movie2, movie_dict, user_dict):
-	same_user_info = []
-	for user1 in movie_dict[movie1].keys():
-		for user2 in movie_dict[movie2].keys():
-			if user1 == user2:
-				user_info = {}
-				user_info["movie1"] = movie_dict[movie1][user1]
-				user_info["movie2"] = movie_dict[movie2][user1]
-				user_info["average"] = user_dict[user1]["average"]
+import pickle
+import threading
 
-				same_user_info.append(user_info)
+class Sim_Matrix:
+	def __init__(self, movie_dict):
+		self.similarity_dict = {}
+		for movie in movie_dict.keys():
+			self.similarity_dict[movie] = {}
 
-	if len(same_user_info) >= THRESHOLD:
-		numerator = 0
-		denom_part1 = 0
-		denom_part2 = 0
-		for user in same_user_info:
-			numerator += (user["movie1"] - user["average"])*(user["movie2"] - user["average"])
-			denom_part1 += (user["movie1"] - user["average"])*(user["movie1"] - user["average"])
-			denom_part2 += (user["movie2"] - user["average"])*(user["movie2"] - user["average"])
+	def similarity(self, movie1, movie2, movie_dict, user_dict):
+		same_user_info = []
+		l1 = list(movie_dict[movie1].keys())
+		l2 = list(movie_dict[movie2].keys())
+		merged = list(set(l1).intersection(l2))
+		for user in merged:
+			user_info = {}
+			user_info["movie1"] = movie_dict[movie1][user]
+			user_info["movie2"] = movie_dict[movie2][user]
+			user_info["average"] = user_dict[user]["average"]
 
-		denom = sqrt(denom_part2) * sqrt(denom_part1)
+			same_user_info.append(user_info)
+		
+		#for user in movie_dict[movie1].keys():
+		#	if user in movie_dict[movie2].keys():
+		#		user_info = {}
+		#		user_info["movie1"] = movie_dict[movie1][user]
+		#		user_info["movie2"] = movie_dict[movie2][user]
+		#		user_info["average"] = user_dict[user]["average"]
+	
+		#		same_user_info.append(user_info)
+	
+		#if len(same_user_info) >= 5:
+		if len(same_user_info) >= 1:
+			numerator = 0
+			denom_part1 = 0
+			denom_part2 = 0
+			for user in same_user_info:
+				numerator += (user["movie1"] - user["average"])*(user["movie2"] - user["average"])
+				denom_part1 += (user["movie1"] - user["average"])*(user["movie1"] - user["average"])
+				denom_part2 += (user["movie2"] - user["average"])*(user["movie2"] - user["average"])
+	
+			denom = pow(denom_part2, .5) * pow(denom_part1, .5)
+	
+			return float(numerator) / float(denom)
+		else:
+			return 0
+	
+	def threaded_similarity_matrix(self, start, end, movie_dict, user_dict):
+		#print("thread started")
+		movie_list = list(movie_dict.keys())
+		for i in range(start, end):
+			for j in range(i, len(movie_list)):
+				print(i, j)
+				sim = self.similarity(movie_list[i], movie_list[j], movie_dict, user_dict)
+				if sim != 0:
+					self.similarity_dict[movie_list[i]][movie_list[j]] = sim
+					self.similarity_dict[movie_list[j]][movie_list[i]] = sim
+		#print("thread ended")
 
-		return float(numerator) / float(denom)
-	else:
-		return 0
+	def create_similarity_matrix(self, movie_dict, user_dict):
+		#print("create_similarity_matrix")
+		splits = [0, 21, 43, 67, 91, 118, 146, 177, 211, 250, 295, 355, 500]
+		#splits = [0, 757, 1549, 2382, 3263, 4200, 5206, 6301, 7512, 8886, 10516, 12640, 17770]
+		threads = []
+		for i in range(len(splits) - 1):
+			threads.append(threading.Thread(target=self.threaded_similarity_matrix, args=(splits[i], splits[i+1], movie_dict, user_dict,)))
+		
+		for thread in threads:
+			thread.start()
 
-def similarity_matrix(movie_dict, user_dict):
-	similarity_dict = {}
-	movie_list = movie_dict.keys()
-	for i in range(len(movie_list)):
-		for j in range(i, len(movie_list)):
-			sim = similarity(movie_list[i], movie_list[j], movie_dict, user_dict)
-			if sim != 0:
-				similarity_dict[movie_list[i]][movie_list[j]] = sim
-				similarity_dict[movie_list[j]][movie_list[i]] = sim
-	return similarity_dict
-
+		for thread in threads:
+			thread.join()
+		#print("all threads joined")
+		
 def weighted_sum(user_id, movie_id, user_dict, similarity_matrix, similarity_threshhold=0.5):
 	# Find all similar movies for that movie
 	similar_movies = set()
@@ -57,3 +93,17 @@ def weighted_sum(user_id, movie_id, user_dict, similarity_matrix, similarity_thr
 		sum_of_similarities += abs(similarity)
 
 	return weighted_user_rating_sum / sim_of_similarities
+
+def pickle_open(filename):
+	f = open(filename, "rb")
+	return pickle.load(f)
+
+if __name__ == "__main__":
+	users = pickle_open("user_movie_ratings.pickle")
+	movies = pickle_open("movie_user_ratings.pickle")
+	sim_matrix = Sim_Matrix(movies)
+	sim_matrix.create_similarity_matrix(movies, users)
+	predicted_rating = weighted_sum(1932640, 14574, users, sim_matrix.similarity_dict)
+	print("Predicted rating for user 1932640 and movie 14574 should be 2, it is: ")
+	print(predicted_rating)
+	#1932640,14574,2
